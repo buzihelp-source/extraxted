af2f4be65a007dfba3be1b846525f3488ef75bb3
buzihelp-source <buzihelp@gmail.com>
2025-12-25 06:49:03 +0530

    Change bot token and API credentials
    Updated sensitive configuration values for the bot.

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6216840269:AAFNzApyRlXuZ94T3UZgd-XqNun2mA40kJU")
-    API_ID = int(os.environ.get("API_ID", "7856100"))
-    API_HASH = os.environ.get("API_HASH", "dba409d7e8d44ab5bb689378deea1792")
-    AUTH_USERS = "1411895712"
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8255874281:AAGyI2bW1XVuzXrLP48qSL1zMdkT9PlZuI4")
+    API_ID = int(os.environ.get("API_ID", "30587265"))
+    API_HASH = os.environ.get("API_HASH", "49a5bcb36e6d5350db387ba444338f37")
+    AUTH_USERS = "7759336095"
+
 
