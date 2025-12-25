c7751734f8c6656024ded7c8a90f2d8a9c65feaf
buzihelp-source <buzihelp@gmail.com>
2025-12-25 09:40:08 +0530

    Refactor Config class environment variable defaults
    Updated environment variable defaults to empty strings.

--- config.py
+++ config.py
@@ -6,9 +6,10 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8255874281:AAGyI2bW1XVuzXrLP48qSL1zMdkT9PlZuI4")
-    API_ID = int(os.environ.get("API_ID", "30587265"))
-    API_HASH = os.environ.get("API_HASH", "49a5bcb36e6d5350db387ba444338f37")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
     AUTH_USERS = "7759336095"
+
 
 
